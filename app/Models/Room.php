9dc11f201ca57d1c6f65626e72a65da6bf7555c2
mason.hows11 <mason.hows11@gmail.com>
2025-12-06 06:53:39 +0330

    add some model & mig updated

---
+++ app/Models/Room.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Room extends Model
+{
+    //
+
+    public function users()
+    {
+         return $this->belongsToMany(User::class,'room_user');
+    }
+
+   
+}
---
+++ routes/channels.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Broadcast;
+
+//Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
+//    return (int) $user->id === (int) $id;
+//});
+
+Broadcast::channel('tasks.{id}', function ($user, $id) {
+    return (int) $user->id === (int) $id;
+});
+
+// parameter name in wildcard channel name
+// & parameter name in function must be same
+// otherwise error
+Broadcast::channel('chat.{roomId}',function($user,$roomId){
+    return $user && $user->rooms()->where('rooms.id',$roomId)->exists();
+    // return $user->rooms->contains($user->id,$roomId);
+});
