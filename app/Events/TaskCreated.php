1ce775867b2eb848a449d6e5e8272690997e7925
mason.hows11 <mason.hows11@gmail.com>
2025-12-06 21:55:16 +0330

    presence channel event  updated 85%

---
+++ app/Events/TaskCreated.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Events;
+
+// use Illuminate\Broadcasting\Channel;
+
+use App\Models\User;
+use Illuminate\Broadcasting\InteractsWithSockets;
+
+// use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class TaskCreated implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $task;
+    private $user;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct($task, User $user)
+    {
+        $this->task = $task;
+        $this->user = $user;
+    }
+
+    public function broadcastAs(): string
+    {
+        return 'task.added';
+    }
+
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            // this is public channel
+            // new Channel('tasks')
+
+            // this is private channel
+            // new PrivateChannel('tasks.'.$this->user->id),
+            new PresenceChannel('tasks.'.$this->user->id),
+            // new PrivateChannel('chat.'.$this->user->id),
+            new PresenceChannel('chat.' . $this->user->id),
+        ];
+    }
+}
