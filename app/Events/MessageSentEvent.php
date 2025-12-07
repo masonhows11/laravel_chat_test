4810a73a0f2f069fbb1ab0a91b865a421ffb859b
mason.hows11 <mason.hows11@gmail.com>
2025-12-07 17:38:42 +0330

    add message sent event

---
+++ app/Events/MessageSentEvent.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\User;
+//use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+//use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class MessageSentEvent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    private $user;
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(User $user)
+    {
+        //
+        $this->user = $user;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            // new PrivateChannel('channel-name'),
+
+            //new PrivateChannel('chat.' . $this->user->id),
+            new PresenceChannel('chat.' . $this->user->id),
+        ];
+    }
+}
