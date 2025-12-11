f34a031fd68e7f4932226bf952f56e6911f3bbee
mason.hows11 <mason.hows11@gmail.com>
2025-12-11 18:26:29 +0330

    updated

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -14,39 +14,31 @@ class ChatController extends Controller
     //
     public function index()
     {
-
         try {
-
             $roomId = auth()->user()->rooms()->first()->id;
             $messages = Message::with('user')
                 ->where('room_id', $roomId)
                 ->orderBy('created_at')
                 ->get();
             return view('chat.room', ['roomId' => $roomId, 'messages' => $messages]);
-
         } catch (\Exception $e) {
-
             return view('errors.404');
         }
-
     }
 
     public function store(Request $request)
     {
         try {
-
             $message = Message::create([
                 'user_id' => auth()->id(),
                 'message' => $request->message,
                 'room_id' => (int)$request->room_id,
             ]);
-
             $roomId = $message->room_id;
             event(new MessageSentEvent($roomId, $message, auth()->user()));
         } catch (\Exception $exception) {
             return response()->json(['error' => $exception->getMessage()]);
         }
-        //return response()->json(['success' => true]);
     }
 
 
@@ -58,13 +50,10 @@ class ChatController extends Controller
         } catch (\Exception $exception) {
             return response()->json(['error' => $exception->getMessage()]);
         }
-
-
     }
 
     public function getTasks()
     {
-
         return response()->json([
             'tasks' => Task::all()
         ], 200);
